51784e10044b6956b2e906dff560a7cd75f0071e
Ivo Araujo <ivo.souza-23@outlook.com>
2025-12-05 13:16:12 -0300

    Iniciando o projeto

---
+++ seasonproject/src/main/java/com/nfl/seasonproject/SeasonprojectApplication.java
@@ -0,0 +1,13 @@
+package com.nfl.seasonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasonprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeasonprojectApplication.class, args);
+	}
+
+}
